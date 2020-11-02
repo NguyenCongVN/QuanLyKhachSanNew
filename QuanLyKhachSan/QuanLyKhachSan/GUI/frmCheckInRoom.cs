0b73cd9e5456fb4b5b5167b298bc67d65f374207
ngoctien <ngoctien18051999@gmail.com>
2020-11-02 21:22:04 +0700

    edit form quan ly khach hang

---
+++ QuanLyKhachSan/QuanLyKhachSan/GUI/frmCheckInRoom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class frmCheckInRoom : Form
+    {
+        public frmCheckInRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #region Hand click button
+
+   
+        private void btn_confim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_CheckIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_removeRoom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+    }
+}
