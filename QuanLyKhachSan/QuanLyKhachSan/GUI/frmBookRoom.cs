608dd9e0ea7e8e277671c5404d050c7eac0220cd
ngoctien <ngoctien18051999@gmail.com>
2020-11-02 21:22:04 +0700

    edit form quan ly khach hang

---
+++ QuanLyKhachSan/QuanLyKhachSan/GUI/frmBookRoom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class frmBookRoom : Form
+    {
+        public frmBookRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void gunaLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
