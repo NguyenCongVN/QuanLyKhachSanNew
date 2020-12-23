35c71f7c691106032ab37602a74c461d69280cce
phamxuantien27 <65357044+phamxuantien27@users.noreply.github.com>
2020-12-23 08:59:24 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Merge pull request #1 from NguyenCongVN/main""""""
    This reverts commit 266aec5ee1d0c21d56946f6238dfe21fa31e5965.

--- QuanLyKhachSan/QuanLyKhachSan/Program.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Program.cs
@@ -4,20 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyKhackSan.DataAcess;
 
 namespace QuanLyKhachSan
 {
-    static class Program
+    public class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        public static ConnectionString ConnectionString = new ConnectionString();
+
+        public static frmLogin frmLogin;
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            frmLogin = new frmLogin();
+            Application.Run(frmLogin);
         }
     }
 }
